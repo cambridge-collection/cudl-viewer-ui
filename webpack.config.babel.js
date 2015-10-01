fe78faef1d86fb460230d4deb2ec240b0be412fb
Hal Blackburn <hwtb2@cam.ac.uk>
2015-10-01 16:20:07 +0100

    Add deps.json resource containing dependency graph of chunks
    This will be used by the server to include common chunks before page chunks
    as required.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -58,6 +58,11 @@ export default {
         // Currently only the standard and advancedsearch pages share content.
         // In the future we may wish to have a global common chunk and a nested
         // one for the standard and advancedsearch pages.
+
+        // NOTE: Update the deps.json file to list dependencies implied by
+        //       the common chunks config if you change it. The java code
+        //       replies on deps.json to know which entry chunks to load in
+        //       what order.
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common',
             filename: 'js/[name]-[chunkhash].js',
