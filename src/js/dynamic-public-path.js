a077a21527b0f08b3741e160c3a5f8add1365536
Hal Blackburn <hwtb2@cam.ac.uk>
2015-10-21 12:12:51 +0100

    Update dynamic-public-path to use CUDL_PRODUCTION_BUILD

--- src/js/dynamic-public-path.js
+++ src/js/dynamic-public-path.js
@@ -1,7 +1,10 @@
 // Dynamically set the output.publicPath in production mode. We don't
 // necessarily know where the webpack resources will be served from, so we
 // allow it to be set via a global var.
-if(window.__cudl_webpack_public_path__) {
+if(CUDL_PRODUCTION_BUILD) {
+    if(typeof window.__cudl_webpack_public_path__ !== 'string')
+        throw new Error('The global __cudl_webpack_public_path__ is not set');
+
     // Webpack handles this assignment specially, it doesn't actually assign
     // a global.
     // See: http://webpack.github.io/docs/configuration.html#output-publicpath
