f6b37ef4c65b4215318891966d491a12abb9b859
Hal Blackburn <hwtb2@cam.ac.uk>
2015-10-27 11:15:05 +0000

    Add inline code from advancedsearch page

--- src/js/pages/advancedsearch.js
+++ src/js/pages/advancedsearch.js
@@ -5,4 +5,24 @@
  */
 
 import '../../css/advancedsearch.css';
+
+import $ from 'jquery';
+import debounce from 'lodash/function/debounce'
+
 import '../base.js';
+
+
+function changeHintDirection() {
+    console.log('changeHintDirection()');
+    // change hint direction
+    if ($(window).width() > 767) {
+        $('span.hint--bottom').addClass('hint--right')
+                              .removeClass('hint--bottom');
+    } else {
+        $('span.hint--right').addClass('hint--bottom')
+                             .removeClass('hint--right');
+    }
+}
+
+$(document).ready(changeHintDirection);
+$(window).resize(debounce(changeHintDirection, 50));
