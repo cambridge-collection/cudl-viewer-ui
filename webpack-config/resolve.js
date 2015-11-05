1302a5132cd497c43d252ef5c1e9f6afabb4c43d
Hal Blackburn <hwtb2@cam.ac.uk>
2015-11-05 17:44:08 +0000

    Allow deps to import cudl-viewer-ui
    This avoids having to create a shared utility package to house common
    stuff. Perhaps a bit of a hack though. Meh.

--- webpack-config/resolve.js
+++ webpack-config/resolve.js
@@ -9,5 +9,9 @@ export default new WebpackConfig().merge({
             rootPath('./bower_components'),
             rootPath('./vendor')
         ],
+        alias: {
+            // Allow dependencies to import modules from here
+            'cudl-viewer-ui': rootPath()
+        }
     }
 });
