cdc6d44414b50df39bf87c7f2db1f70299eef6c7
Hal Blackburn <hwtb2@cam.ac.uk>
2015-11-11 16:14:34 +0000

    Add gzip'd versions of production assets
    The cudl-viewer can then serve assets with gzip compression without
    doing dynamic compression.

---
+++ webpack-config/plugins-production.js
@@ -0,0 +1,19 @@
+import { gzip, Z_BEST_COMPRESSION } from 'zlib';
+
+import WebpackConfig from 'webpack-config';
+import CompressionPlugin from 'compression-webpack-plugin';
+
+
+function gzipBest(buf, callback) {
+    return gzip(buf, {level: Z_BEST_COMPRESSION}, callback);
+}
+
+export default new WebpackConfig().merge({
+    plugins: [
+        new CompressionPlugin({
+            asset: "{file}.gz",
+            algorithm: gzipBest,
+            minRatio: 0.8
+        })
+    ]
+});
--- webpack-config/plugins.js
+++ webpack-config/plugins.js
@@ -6,33 +6,37 @@ import { env } from 'cudl-webpack-config/lib/util';
 import { rootPath, resolver } from './paths';
 
 
-export default new WebpackConfig().merge({
-    plugins: [
-        // Currently only the standard and advancedsearch pages share content.
-        // In the future we may wish to have a global common chunk and a nested
-        // one for the standard and advancedsearch pages.
+let pwd = resolver(__dirname);
 
-        // NOTE: Update the deps.json file to list dependencies implied by
-        //       the common chunks config if you change it. The java code
-        //       replies on deps.json to know which entry chunks to load in
-        //       what order.
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common',
-            filename: env('cudl-viewer-ui.filenameTemplateJsChunk'),
-            // FIXME: determine sensible common chunks
-            chunks: [ 'page-standard', 'page-advancedsearch', 'page-login']
-        }),
-        new AssetsPlugin({
-            filename: env('cudl-viewer-ui.assetJsonFilename'),
-            path: env('cudl-viewer-ui.assetJsonPath'),
-            prettyPrint: true
-        }),
-        new webpack.DefinePlugin({
-            // Relative to the public path
-            CKEDITOR_LOCATION:
-                JSON.stringify(env('cudl-viewer-ui.ckeditorLocation')),
-            CUDL_PRODUCTION_BUILD:
-                JSON.stringify(env('cudl-viewer-ui.env') === 'production')
-        })
-    ]
-});
+export default new WebpackConfig()
+    .merge({
+        plugins: [
+            // Currently only the standard and advancedsearch pages share content.
+            // In the future we may wish to have a global common chunk and a nested
+            // one for the standard and advancedsearch pages.
+
+            // NOTE: Update the deps.json file to list dependencies implied by
+            //       the common chunks config if you change it. The java code
+            //       replies on deps.json to know which entry chunks to load in
+            //       what order.
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common',
+                filename: env('cudl-viewer-ui.filenameTemplateJsChunk'),
+                // FIXME: determine sensible common chunks
+                chunks: [ 'page-standard', 'page-advancedsearch', 'page-login']
+            }),
+            new AssetsPlugin({
+                filename: env('cudl-viewer-ui.assetJsonFilename'),
+                path: env('cudl-viewer-ui.assetJsonPath'),
+                prettyPrint: true
+            }),
+            new webpack.DefinePlugin({
+                // Relative to the public path
+                CKEDITOR_LOCATION:
+                    JSON.stringify(env('cudl-viewer-ui.ckeditorLocation')),
+                CUDL_PRODUCTION_BUILD:
+                    JSON.stringify(env('cudl-viewer-ui.env') === 'production')
+            })
+        ]
+    })
+    .extend(pwd('./plugins-[env].js'));
