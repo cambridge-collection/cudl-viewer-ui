5a12b9b643faf24a5709e86a9bb0b4738dece21d
Hal Blackburn <hwtb2@cam.ac.uk>
2015-11-12 10:45:46 +0000

    Update pages using common chunk
    Everything apart from the document and transcription pages now share
    common modules by placing them in the common chunk.

--- webpack-config/plugins.js
+++ webpack-config/plugins.js
@@ -1,9 +1,11 @@
+import without from 'lodash/array/without';
 import AssetsPlugin from 'assets-webpack-plugin';
 import webpack from 'webpack';
 import WebpackConfig from 'webpack-config';
 import { env } from 'cudl-webpack-config/lib/util';
 
 import { rootPath, resolver } from './paths';
+import entryConfig from './entry';
 
 
 let pwd = resolver(__dirname);
@@ -11,9 +13,11 @@ let pwd = resolver(__dirname);
 export default new WebpackConfig()
     .merge({
         plugins: [
-            // Currently only the standard and advancedsearch pages share content.
-            // In the future we may wish to have a global common chunk and a nested
-            // one for the standard and advancedsearch pages.
+            // Every page apart from document and transcription share a fair
+            // amount of stuff, so break out that stuff into a shared chunk.
+            // There's scope to create a additional shared chunks if necessary.
+            // For example, if the similarity and tagging code is loaded on
+            // demand, there may be value in them having a shared chunk.
 
             // NOTE: Update the deps.json file to list dependencies implied by
             //       the common chunks config if you change it. The java code
@@ -22,8 +26,9 @@ export default new WebpackConfig()
             new webpack.optimize.CommonsChunkPlugin({
                 name: 'common',
                 filename: env('cudl-viewer-ui.filenameTemplateJsChunk'),
-                // FIXME: determine sensible common chunks
-                chunks: [ 'page-standard', 'page-advancedsearch', 'page-login']
+                chunks: without(
+                    Object.keys(entryConfig.entry),
+                    'page-document', 'page-transcription')
             }),
             new AssetsPlugin({
                 filename: env('cudl-viewer-ui.assetJsonFilename'),
