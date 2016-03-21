055b9c371201039c0aa6c295a68608bc2a1ae810
Hal Blackburn <hwtb2@cam.ac.uk>
2016-03-21 12:17:36 +0000

    Add optional support for zopfli compression
    If the node-zopfli library is installed it'll be used instead of zlib
    to perform gzip compression of production assets.

--- webpack-config/plugins-production.js
+++ webpack-config/plugins-production.js
@@ -1,18 +1,26 @@
 import { gzip, Z_BEST_COMPRESSION } from 'zlib';
+import optional from 'optional';
+let zopfli = optional('node-zopfli');
 
 import WebpackConfig from 'webpack-config';
 import CompressionPlugin from 'compression-webpack-plugin';
 
-
-function gzipBest(buf, callback) {
+function compressZlibGzip(buf, callback) {
     return gzip(buf, {level: Z_BEST_COMPRESSION}, callback);
+}
+
+function compressZopfliGzip(buf, callback) {
+    return zopfli.gzip(buf, callback);
 }
 
 export default new WebpackConfig().merge({
     plugins: [
         new CompressionPlugin({
             asset: "[file].gz",
-            algorithm: gzipBest,
+            // Use Zopfli to gzip compress if it's available, otherwise zlib.
+            // Zopfli compresses slightly better (0-10%) than zlib, but is much
+            // slower to perform the compression.
+            algorithm: zopfli ? compressZopfliGzip : compressZlibGzip,
             minRatio: 0.8
         })
     ]
