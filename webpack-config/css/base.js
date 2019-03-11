2a9e21716f730b645dc49507efb8cc82d3a4cf27
jlf44 <jlf44@cam.ac.uk>
2019-03-11 13:01:26 +0000

    Updating the chunking and webpack configuration

--- webpack-config/css/base.js
+++ webpack-config/css/base.js
@@ -4,7 +4,6 @@ import webpack from 'webpack';
 export default new Config().merge({
     plugins: [
         new webpack.LoaderOptionsPlugin({
-            // test: /\.xxx$/, // may apply this only for some modules
             options: {
                 postcss: [require('autoprefixer')]
             }
