5dfaf86d2ae5fababef165f4fed7c5dfaadc3c39
Hal Blackburn <hwtb2@cam.ac.uk>
2020-02-27 13:06:42 +0000

    Update dependency major versions where possible
    spin.js required some minor changes in usage, but the rest are working
    unchanged. bootstrap is not updated as 4.x is a significant update.

--- src/js/pages/advanced-search-results.js
+++ src/js/pages/advanced-search-results.js
@@ -6,7 +6,7 @@ import $ from 'jquery';
 import 'jquery.easing';
 import 'jquery-paging';
 import defer from 'lodash/defer';
-import Spinner from 'spin.js';
+import {Spinner} from 'spin.js';
 import 'bootstrap-slider';
 
 import '../base.js';
@@ -392,27 +392,25 @@ function requery(state) {
 function getSpinner() {
     // Setup spinner.
     var opts = {
-      lines: 13 // The number of lines to draw
-    , length: 0 // The length of each line
-    , width: 27 // The line thickness
-    , radius: 63 // The radius of the inner circle
-    , scale: 1 // Scales overall size of the spinner
-    , corners: 1 // Corner roundness (0..1)
-    , color: '#000' // #rgb or #rrggbb or array of colors
-    , opacity: 0.1 // Opacity of the lines
-    , rotate: 0 // The rotation offset
-    , direction: 1 // 1: clockwise, -1: counterclockwise
-    , speed: 1.5 // Rounds per second
-    , trail: 44 // Afterglow percentage
-    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-    , zIndex: 2e9 // The z-index (defaults to 2000000000)
-    , className: 'spinner' // The CSS class to assign to the spinner
-    , top: '50%' // Top position relative to parent
-    , left: '50%' // Left position relative to parent
-    , shadow: true // Whether to render a shadow
-    , hwaccel: true // Whether to use hardware acceleration
-    , position: 'fixed' // Element positioning
-    }
+        lines: 13, // The number of lines to draw
+        length: 0, // The length of each line
+        width: 27, // The line thickness
+        radius: 63, // The radius of the inner circle
+        scale: 1, // Scales overall size of the spinner
+        corners: 1, // Corner roundness (0..1)
+        color: '#000', // CSS color or array of colors
+        fadeColor: 'transparent', // CSS color or array of colors
+        speed: 1.5, // Rounds per second
+        rotate: 0, // The rotation offset
+        animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        className: 'spinner', // The CSS class to assign to the spinner
+        top: '50%', // Top position relative to parent
+        left: '50%', // Left position relative to parent
+        shadow: '0 0 1px transparent', // Box-shadow for the lines
+        position: 'fixed' // Element positioning
+    };
 
     return new Spinner(opts);
 }
