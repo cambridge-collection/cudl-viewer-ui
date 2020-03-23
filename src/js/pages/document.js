cee7d99d4956fcb1eb4fbc3a17904cef91ebd59d
Mary Chester-Kadwell <mec31@cam.ac.uk>
2020-03-23 16:20:46 +0000

    Show/hide OpenSeadragon viewport navigator

--- src/js/pages/document.js
+++ src/js/pages/document.js
@@ -47,6 +47,8 @@ import { registerCsrfPrefilter } from '../ajax-csrf';
     iiifMiradorURL
     itemAuthors - not used
     itemAuthorsFullForm - not used
+
+    viewportNavigatorEnabled
  */
 
 let context;
@@ -209,8 +211,14 @@ function updateAddThisShareUrl(url = getCanonicalUrl()) {
     }
 }
 
+// Viewport navigator is a mini viewer that appears inside the main OpenSeadragon viewer
+function showViewportNavigator() {
+    return !!context.viewportNavigatorEnabled;
+}
+
 function setupSeaDragon(data) {
     OpenSeadragon.setString("Tooltips.Home", "Reset zoom");
+    let showNav = showViewportNavigator();
     viewer = new OpenSeadragon.Viewer({
         id : "doc",
         debugMode : false,
@@ -223,9 +231,9 @@ function setupSeaDragon(data) {
         rotateRightButton : "rotateRight",
         fullPageButton: "fullscreen",
         maxZoomPixelRatio: 1,
-
+        showNavigator: showNav,
+        navigatorPosition: "TOP_LEFT",
     });
-    viewer.clearControls(); // hides controls.
 
     // Setup forward and backward buttons
     function nextPage() {
