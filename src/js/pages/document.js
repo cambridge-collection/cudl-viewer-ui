d3fbf9c82cb5cba8859125b82ed4fa0379bab4ff
Mary Chester-Kadwell <mec31@cam.ac.uk>
2020-04-09 15:20:34 +0100

    Reverse direction of thumbnails for items with R-to-L text direction.

--- src/js/pages/document.js
+++ src/js/pages/document.js
@@ -576,7 +576,13 @@ function showThumbnailPage(pagenum) {
             }
             if (i == startIndex || ((i) % props.MAX_THUMBNAIL_ITEMS_ON_ROW) == 0) {
 
-                thumbnailhtml = thumbnailhtml.concat("<div class='row'>");
+                if (typeof data.textDirection !== 'undefined' && data.textDirection === 'R'){
+
+                    thumbnailhtml = thumbnailhtml.concat("<div class='row row-right-to-left'>");
+
+                } else {
+                    thumbnailhtml = thumbnailhtml.concat("<div class='row'>");
+                }
             }
 
             thumbnailhtml = thumbnailhtml
