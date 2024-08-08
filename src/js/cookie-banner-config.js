45ace982a52b94a6f9f7576261fabc56e916e38f
Michael Hawkins <mjh39@cam.ac.uk>
2024-08-08 16:23:55 +0100

    fix: improve cookie text and style

--- src/js/cookie-banner-config.js
+++ src/js/cookie-banner-config.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function(){
     window.cookieconsent.initialise({
         revokeBtn: '<div class="cc-revoke"></div>',
         type: "opt-in",
-        position: "top",
+        position: "bottom",
         theme: "classic",
         palette: {
             popup: {
@@ -17,7 +17,7 @@ window.addEventListener('load', function(){
             }
         },
         content: {
-            message: "This website uses cookies.",
+            message: "<div class=\"cookie-text-container\"><p class=\"cookie-head\">This website uses cookies.</p><p>Cookies are little files that we save on your device to remember your preferences. We use <b>necessary</b> cookies to make our site work. We use <b>site usage measurement</b> cookies to analyse anonymised usage patterns, to make our websites better for you.</p></div>",
             link: "Privacy Statement",
             href: "/help#cookies"
         },
