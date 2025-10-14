c20e0d8c38c65a480cfa57e1e4893ef6ff51b938
Michael Hawkins <mjh39@cam.ac.uk>
2025-10-14 08:29:30 +0100

    fix(missing media-embed): add missing media-embed lib

---
+++ src/js/pages/media-embed.js
@@ -0,0 +1,5 @@
+import '../mediaEmbed/mediaEmbed.min.css'
+import '../mediaEmbed/mediaEmbed.min'
+
+console.log('media-embed script running');
+window.configureMediaEmbedConsentCookie?.('cookieconsent_status', 'allow');
